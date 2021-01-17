182990f0b8c3cd929d183d7c532d3ba39a1da233
Rithvik <rbhogavilli@gmail.com>
2021-01-16 20:27:20 -0500

    nvm i have so much to learn

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.scss';
 
 function App() {
   return (
-    <div class="intro">
-    <h1>My name is <div class="name">Rithvik Bhogavilli</div>!</h1>
-    <h2>I'm a senior at Poolesville High School in the SMCS program.</h2>
+    <div class="wrapper flex">
+      <div class="intro flex">
+        <h1>My name is <div class="name">Rithvik Bhogavilli</div>!</h1>
+        <h2>I'm a senior in the SMCS program at Poolesville High School.</h2>
+      </div>
     </div>
   );
 }
