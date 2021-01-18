933bf8de57ec05a717b532743faad897bf22d801
Rithvik <rbhogavilli@gmail.com>
2021-01-17 22:26:59 -0500

    stuff

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
         <h1>My name is <div class="name">Rithvik Bhogavilli</div>!</h1>
         <h2>I'm a senior in the SMCS program at Poolesville High School.</h2>
       </div>
+      <hr width="100rem"/>
+      <div class="about">
+        <h1>About Me</h1>
+        <p>Hi there, I guess you found me. I'm Rithvik and I'm a senior in high school. I do robotics, cybersecurity, and engineering. If you are good at web development, please help me.</p>
+      </div>
     </div>
   );
 }
