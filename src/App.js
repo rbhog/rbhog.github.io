c29cf1ab2588dbf9d503af4df297fb1c762dae79
Rithvik <rbhogavilli@gmail.com>
2021-05-06 21:45:44 -0400

    god save the queen

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import './App.scss';
 
 function App() {
   return (
-    <div class="wrapper flex">
-      <div class="intro flex">
+    <div class="wrapper">
+      <div class="intro">
         <h1>My name is <div class="name">Rithvik Bhogavilli</div>!</h1>
         <h2>I'm a senior in the SMCS program at Poolesville High School.</h2>
+        <hr width="100rem"/>
       </div>
-      <hr width="100rem"/>
       <div class="about">
-        <h1>About Me</h1>
-        <p>Hi there, I guess you found me. I'm Rithvik and I'm a senior in high school. I do robotics, cybersecurity, and engineering. If you are good at web development, please help me.</p>
+        <div class="about_face">
+          <img 
+            style={{
+              borderRadius: "50%",
+              width: "256px",
+              height: "256px",
+              objectFit: "cover"
+            }}
+            src="/tomas.png"></img>
+        </div>
+        <div class="about_splitter"></div>
+        <div class="about_blurb">
+          <p>Hi there, I guess you found me. I'm Rithvik and I'm a senior in high school. I do robotics, cybersecurity, and engineering. If you are good at web development, please help me.</p>
+        </div>
       </div>
     </div>
   );
